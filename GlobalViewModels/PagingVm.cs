fa6b0f66efe69672a9016613daf94dfcd17a2b85
SamarMohammedM <88279180+SamarMohammedM@users.noreply.github.com>
2021-09-06 17:05:39 +0200

    Create PagingVm.cs

---
+++ GlobalViewModels/PagingVm.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utilities.GlobalViewModels
+{
+    public class PagingVm<T> where T : class
+    {
+        public List<T> Model { get; set; }
+        public int TotalCount { get; set; }
+        public int TotalCountInPages { get; set; }
+    }
+}
