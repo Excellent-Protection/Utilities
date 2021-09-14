1f53adcc22ce9d25af534083754fcec4571ddc3c
EmanShahin <90183165+EmanShahin@users.noreply.github.com>
2021-09-14 11:16:20 +0200

    changes

---
+++ Helpers/DateTimeConvertor.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utilities.Helpers
+{
+    public class DateTimeConvertor : DateTimeConverterBase
+    {
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            if (reader.Value != null)
+            {
+                var cultureInfo = new CultureInfo("en-AU");
+                var date = DateTime.ParseExact(reader.Value.ToString(), "dd/MM/yyyy", cultureInfo);
+                return date;
+            }
+            return null;
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            writer.WriteValue(((DateTime)value).ToString("dd/MM/yyyy"));
+        }
+    }
+}
