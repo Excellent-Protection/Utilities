71253a0a716cb0d04f159d54e6ab60351a6917ea
SamarMohammedM <88279180+SamarMohammedM@users.noreply.github.com>
2021-09-22 10:16:42 +0200

    Update SmsManager.cs

--- GlobalManagers/CRM/SmsManager.cs
+++ GlobalManagers/CRM/SmsManager.cs
@@ -16,72 +16,35 @@ using Recruitment.DataAccess;
 
 namespace Recruitment.Manager
 {
-    public class SmsManager : IDisposable 
+    public class SmsManager : IDisposable
     {
         public void Dispose()
         {
 
         }
-   
-        public bool Send(string Message, string MobileNumber )
+
+
+        private static bool CreateSms(string Message, string MobileNumber, string Regarding = null, string EntityName = null)
         {
+
             try
             {
-                //Snd To SMS 
-
-                SettingsManager _settingsManager = new SettingsManager();
-                bool IsTestMode = bool.Parse(_settingsManager["IsTestMode"].Value);
-                if (!IsTestMode)
+                var SMS = new CrmSms();
+                SMS.MobileNumbers = MobileNumber;
+                SMS.MessageText = Message;
+                if (!string.IsNullOrEmpty(Regarding) && !string.IsNullOrEmpty(EntityName))
                 {
-                    string AppId = _settingsManager["SmsAppId"].Value;
-
-                    string TagName = _settingsManager["TagName"].Value;
-                    string fullUrl = "http://basic.unifonic.com/wrapper/sendSMS.php?to=" + MobileNumber + "&msg=" + Message + "&sender=" + TagName + "&appsid=" + AppId;
-                    string request = fullUrl;
-
-                    WebRequest webReq = WebRequest.Create(request);
-                    webReq.Method = "GET";
-                    byte[] byteArray = Encoding.UTF8.GetBytes(request);
-                    webReq.ContentType = "application/x-www-form-urlencoded";
-                    WebResponse webResponse = webReq.GetResponse();
-                    var responseString = new StreamReader(webResponse.GetResponseStream()).ReadToEnd();
-                    webResponse.Close();
+                    SMS.Regarding = new EntityReference(EntityName, new Guid(Regarding));
                 }
+                CRMService.Get.Create(SMS);
                 return true;
-               
-
             }
             catch (Exception ex)
             {
-                LogError.Error(ex, System.Reflection.MethodBase.GetCurrentMethod().Name, ("Message", Message), ("MobileNumber", MobileNumber));
+                LogError.Error(ex, System.Reflection.MethodBase.GetCurrentMethod().Name, ("Message", Message), ("MobileNumber", MobileNumber), ("Regarding", Regarding), ("EntityName", EntityName));
                 return false;
             }
-            
-        }
 
-        public void CreateSms(string Message, string MobileNumber , string Regarding=null, string EntityName=null) {
-
-            try
-            {
-                SettingsManager _settingsManager = new SettingsManager();
-                bool IsTestMode = bool.Parse(_settingsManager["IsTestMode"].Value);
-                if (!IsTestMode)
-                {
-                    var SMS = new CrmSms();
-                    SMS.MobileNumbers = MobileNumber;
-                    SMS.MessageText = Message;
-                    if (!string.IsNullOrEmpty(Regarding) && !string.IsNullOrEmpty(EntityName))
-                    {
-                        SMS.Regarding = new EntityReference(EntityName, new Guid(Regarding));
-                    }
-                CRMService.Get.Create(SMS);
-                }
-            }
-            catch(Exception ex)
-            {
-                LogError.Error(ex, System.Reflection.MethodBase.GetCurrentMethod().Name, ("Message", Message), ("MobileNumber", MobileNumber),("Regarding", Regarding),("EntityName", EntityName));
-            }
-                
         }
 
         /// <summary>
@@ -90,14 +53,15 @@ namespace Recruitment.Manager
         /// <param name="Smscode"></param>
         /// <param name="EntityID"> @id param in query if exist</param>
         /// <returns></returns>
-        public static string GetSmsText(string Smscode,string EntityID=null,Dictionary<string,string> Item=null)
+        private static string GetSmsText(string Smscode, string EntityID = null, Dictionary<string, string> Item = null)
         {
 
             var Code = new QueryExpression(CrmEntityNamesMapping.SmsText);
             Code.ColumnSet = new ColumnSet("new_smsbody", "new_sqlquery");
             Code.Criteria.AddCondition("new_code", ConditionOperator.Equal, Smscode);
-            var Sms= CRMService.Get.RetrieveMultiple(Code).Entities.FirstOrDefault();
-            if (Sms != null) {
+            var Sms = CRMService.Get.RetrieveMultiple(Code).Entities.FirstOrDefault();
+            if (Sms != null)
+            {
                 string SmsBody = Sms["new_smsbody"].ToString();
                 if (Sms.Attributes.Contains("new_sqlquery") && !string.IsNullOrEmpty(Sms["new_sqlquery"].ToString()))
                 {
@@ -113,18 +77,35 @@ namespace Recruitment.Manager
                 }
                 if (Sms.Attributes.Contains("new_smsbody") && !string.IsNullOrEmpty(Sms["new_smsbody"].ToString()))
                 {
-                    foreach ( KeyValuePair<string,string>  Keys in Item) {
+                    foreach (KeyValuePair<string, string> Keys in Item)
+                    {
                         SmsBody = SmsBody.Replace("@" + Keys.Key, Keys.Value);
                     }
 
-      
+
                 }
                 return SmsBody;
             }
-            else {
+            else
+            {
                 return null;
             }
         }
 
+        /// <summary>
+        /// get Sms Text from crm replace keywords then create new sms record
+        /// </summary>
+        /// <param name="Smscode"></param>
+        /// <param name="MobileNumber"></param>
+        /// <param name="RegardingEntityId"></param>
+        /// <param name="RegardingEntityName"></param>
+        /// <param name="EntityID"></param>
+        /// <param name="KeyToReplace"></param>
+        /// <returns></returns>
+        public static bool SendSms(string Smscode, string MobileNumber, Dictionary<string, string> KeyToReplace = null, string RegardingEntityId = null, string RegardingEntityName = null, string EntityID = null)
+        {
+            var Msg = GetSmsText(Smscode, EntityID, KeyToReplace);
+            return CreateSms(Msg, MobileNumber, RegardingEntityId, RegardingEntityName);
+        }
     }
 }
