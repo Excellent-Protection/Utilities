38978694babc3044d30d3f303e687fdc5d582a9a
SamarMohammedM <88279180+SamarMohammedM@users.noreply.github.com>
2021-10-28 15:56:33 +0200

    migrations

---
+++ Migrations/202110281000057_paymentPlatform.cs
@@ -0,0 +1,18 @@
+﻿namespace Utilities.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class paymentPlatform : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.paymentMethods", "Platforms", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.paymentMethods", "Platforms");
+        }
+    }
+}
---
+++ Migrations/202110281149381_IsTestPayment.cs
@@ -0,0 +1,18 @@
+﻿namespace Utilities.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class IsTestPayment : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.PaymentTransactions", "IsTest", c => c.Boolean(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.PaymentTransactions", "IsTest");
+        }
+    }
+}
