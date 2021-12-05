c9f1d90088a180e760674a7dd364e6f6df6fa5a1
mohamedatiah97 <m.atiah@excp.sa>
2021-12-05 11:35:28 +0200

    Skill repository and Manager

---
+++ GlobalViewModels/Custom/EmployeeSkillsDetailVM.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utilities.GlobalViewModels.Custom
+{
+    public class SkillsVm
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Icone { get; set; }
+        public string EmployeeId { get; set; }
+        public int? Category { get; set; }
+    }
+}
