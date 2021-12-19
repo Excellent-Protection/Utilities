7759eb2b1a9c8228fbbc477a51cc858e4834f0f2
EmanShahin <90183165+EmanShahin@users.noreply.github.com>
2021-12-19 10:28:12 +0200

    Hourly Service

---
+++ GlobalManagers/CRM/ServiceManager.cs
@@ -0,0 +1,119 @@
+﻿using HourlySectorLib.Repositories;
+using HourlySectorLib.ViewModels;
+using HourlySectorLib.ViewModels.Custom;
+using Microsoft.Xrm.Sdk;
+using Models.CRM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Utilities;
+using Utilities.Defaults;
+using Utilities.Enums;
+using Utilities.GlobalManagers;
+using Utilities.GlobalViewModels;
+using Utilities.Helpers;
+using Utilities.Mappers;
+using Westwind.Globalization;
+
+namespace HourlySectorLib.Managers
+{
+    public class ServiceManager : BaseManager, IDisposable
+    {
+        ServiceRepository _repo;
+        public ServiceManager(RequestUtility requestUtility):base(requestUtility)
+        {
+            _repo = new ServiceRepository(RequestUtility);
+        }
+
+       
+
+
+        public ResponseVm<List<BaseQuickLookupVm>> GetServiceResourceGroups(string serviceId)
+        {
+            try
+            {
+                var service = _repo.GetServiceResourceGroups(serviceId);
+                var resourceGroups = service.RelatedEntities.Contains(new Relationship(CrmRelationsNameMapping.Service_ResourceGroupOneToMany)) ? service.RelatedEntities[new Relationship(CrmRelationsNameMapping.Service_ResourceGroupOneToMany)].Entities.Select(z => z.ToEntity<ResourceGroup>()).Where(d => d.statecode == 0).ToModelListData<BaseQuickLookupVm>() : null;
+                return new ResponseVm<List<BaseQuickLookupVm>> { Status = HttpStatusCodeEnum.Ok, Data = resourceGroups.ToList() };
+            }
+            catch(Exception ex)
+            {
+
+            }
+            return new ResponseVm<List<BaseQuickLookupVm>> { Status = HttpStatusCodeEnum.IneternalServerError, Message = DbRes.T("AnErrorOccurred", "Shared") };
+
+        }
+
+        public ResponseVm<List<DisplayServiceVm>> GetServicesForService(ServiceType serviceType)
+        {
+            IEnumerable<Service> services = null;
+            switch(serviceType)
+            {
+                case ServiceType.Hourly:
+                    services = _repo.GetHourlyServices(DefaultValues.HourlyServiceProjectId);
+                    break;
+            }
+            var servicesList = services.ToModelListData<DisplayServiceVm>().ToList();
+            return new ResponseVm<List<DisplayServiceVm>> { Status = HttpStatusCodeEnum.Ok, Data = servicesList };
+        }
+        public int GetServiceDisplayCitiesOption(string serviceId)
+        {
+            var result = _repo.GetServiceDisplayCitiesOption(serviceId);
+            return result;
+        }
+
+        public  List<int?> GetNumOfWorkers(string serviceId)
+        {
+            try
+            {
+                var serviceLaborNumbers = _repo.GetMaxMinEmployeeNumber(serviceId);
+                return new List<int?> { serviceLaborNumbers.MinLaborNumber , serviceLaborNumbers.MaxLaborNumber};
+            }
+            catch(Exception ex)
+            {
+                LogError.Error(ex, System.Reflection.MethodBase.GetCurrentMethod().Name);
+            }
+            return null;
+
+        }
+
+        public List<string> GetServiceHours(string serviceId)
+        {
+            try
+            {
+                var service= _repo.GetServiceHoursNumbers(serviceId);
+                var serviceHours = service != null ? service.ServiceHours.Split(',').ToList() : new List<string>();
+                return serviceHours;
+            }
+            catch (Exception ex)
+            {
+                LogError.Error(ex, System.Reflection.MethodBase.GetCurrentMethod().Name);
+            }
+            return null;
+
+        }
+        public List<string> GetServiceShifts(string serviceId)
+        {
+            try
+            {
+                var service = _repo.GetServiceShifts(serviceId);
+                var serviceShifts = service != null ? service.ServiceShifts.Split(',').ToList() : new List<string>();
+                using (GlobalManager _mngr = new GlobalManager(RequestUtility))
+                {
+                    return serviceShifts;
+                }
+            }
+            catch (Exception ex)
+            {
+                LogError.Error(ex, System.Reflection.MethodBase.GetCurrentMethod().Name);
+            }
+            return null;
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
---
+++ GlobalRepositories/CRM/ServiceRepository.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Messages;
+using Microsoft.Xrm.Sdk.Query;
+using Models.CRM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Utilities;
+using Utilities.DataAccess.CRM;
+using Utilities.Defaults;
+using Utilities.Enums;
+using Utilities.Helpers;
+
+namespace HourlySectorLib.Repositories
+{
+   public class ServiceRepository : BaseCrmEntityRepository
+    {
+        public ServiceRepository(RequestUtility requestUtility):base(requestUtility)
+        {
+                
+        }
+
+
+        public Service GetServiceShifts(string serviceId)
+        {
+            var _service = CRMService.Service;
+            var service = _service.Retrieve(CrmEntityNamesMapping.Service, new Guid(serviceId), new ColumnSet("new_serviceshifts")).ToEntity<Service>();
+            return service;
+        }
+
+        public Service GetServiceResourceGroups(string serviceId)
+        {
+            var _service = CRMService.Service;
+            RelationshipQueryCollection relatedEntityCollection = new RelationshipQueryCollection();
+            //resourceGroupRelation
+            relatedEntityCollection.Add(new Relationship(CrmRelationsNameMapping.Service_ResourceGroupOneToMany),
+                new QueryExpression()
+                {
+                    EntityName = CrmEntityNamesMapping.ResourceGroup,
+                    ColumnSet = new ColumnSet(true)
+                });
+            RetrieveRequest request = new RetrieveRequest()
+            {
+                RelatedEntitiesQuery = relatedEntityCollection,
+                Target = new EntityReference
+                {
+                    Id = new Guid(serviceId),
+                    LogicalName = CrmEntityNamesMapping.Service
+                }
+            };
+            RetrieveResponse response = ((RetrieveResponse)_service.Execute(request));
+            var Service = response.Entity.ToEntity<Service>();
+            return Service;
+
+        }
+
+        public IEnumerable<Service> GetHourlyServices(string projectId)
+        {
+            var _service = CRMService.Service;
+            var query = new QueryExpression(CrmEntityNamesMapping.Service);
+            query.Criteria.AddCondition("new_projectid", ConditionOperator.Equal, projectId);
+            query.Criteria.AddCondition("statecode", ConditionOperator.Equal, (int)CrmEntityState.Active);
+            query.ColumnSet = new ColumnSet("new_serviceid", "new_servicenamearabic", "new_serviceenglishname", "new_arabicdescription", "new_englishdescription", "new_icon");
+            return _service.RetrieveMultiple(query).Entities.Select(a => a.ToEntity<Service>());
+
+        }
+        public int GetServiceDisplayCitiesOption(string serviceId)
+        {
+            var _service = CRMService.Service;
+            var service = _service.Retrieve(CrmEntityNamesMapping.Service, new Guid(serviceId), new ColumnSet("new_displaycities")).ToEntity<Service>();
+            return (service != null && service.DisplayCities != null) ? service.DisplayCities.Value : -1;
+        }
+        public Service GetMaxMinEmployeeNumber(string serviceId)
+        {
+            var _service = CRMService.Service;
+            var service = _service.Retrieve(CrmEntityNamesMapping.Service, new Guid(serviceId), new ColumnSet("new_maxlabornumber", "new_minlabornumber")).ToEntity<Service>();
+            return service;
+        }
+
+        public Service GetServiceHoursNumbers(string serviceId)
+        {
+            var _service = CRMService.Service;
+            var service = _service.Retrieve(CrmEntityNamesMapping.Service, new Guid(serviceId), new ColumnSet("new_servicehours")).ToEntity<Service>();
+            return service;
+        }
+
+
+        public Service GetServiceShift(string serviceId)
+        {
+            var _service = CRMService.Service;
+            var service = _service.Retrieve(CrmEntityNamesMapping.Service, new Guid(serviceId), new ColumnSet("new_serviceshifts")).ToEntity<Service>();
+            return service;
+        }
+    }
+}
