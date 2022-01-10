b26e2285f6e8bdeb181605edb51e507479a5927e
EmanShahin <90183165+EmanShahin@users.noreply.github.com>
2022-01-10 17:21:33 +0200

    Revert "Merge branch 'Pre-Production' into Production"
    This reverts commit aed0e531a9ccae856fa852ad49958f31b89b6a00, reversing
    changes made to d063ef457aa9dd9125812f3929a8da1023cc2955.

--- Configuration/Identity/PaymentTransactionConfiguration.cs
+++ Configuration/Identity/PaymentTransactionConfiguration.cs
@@ -1,8 +1,6 @@
 ﻿using Models.Labor;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.Data.Entity.Infrastructure.Annotations;
 using System.Data.Entity.ModelConfiguration;
 using System.Linq;
 using System.Text;
@@ -24,10 +22,7 @@ namespace Utilities.DataAccess.Configuration.Identity
             Property(a => a.ContractId).HasMaxLength(250);
             Property(a => a.EntityName).HasMaxLength(250);
             Property(a => a.TransactionTypeName).HasMaxLength(50);
-            Property(a => a.MerchantTransactionId)
-                .HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity)
-                .HasColumnAnnotation("Index", new IndexAnnotation(new[] { new IndexAttribute("Index") { IsUnique = true } }))
-                ;
+            Property(a => a.MerchantTransactionId).HasMaxLength(250);
             Property(a => a.PaymentStatus).HasMaxLength(20);
             Property(a => a.PaymentStatusName).HasMaxLength(250);
             Property(a => a.SourceOfFundsIssuer).HasMaxLength(250);
