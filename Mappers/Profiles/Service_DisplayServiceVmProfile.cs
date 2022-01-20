81f96764bdded7be1e07bf5d0f46fd1db46556db
EmanMamdouhShaker <eman.mamdoh@excp.sa>
2022-01-20 16:37:20 +0200

    serviceforservice

---
+++ Mappers/Profiles/Service_DisplayServiceVmProfile.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using HourlySectorLib.ViewModels.Custom;
+using Models.CRM;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Utilities.GlobalViewModels.Custom;
+using Utilities.Mappers.Resolvers;
+
+namespace Utilities.Mappers.Profiles
+{
+    public class Service_DisplayServiceVmProfile : Profile
+    {
+
+        public Service_DisplayServiceVmProfile()
+        {
+            CreateMap<DisplayServiceVm, Service>()
+
+           .IgnoreAllPropertiesWithAnInaccessibleSetter()
+           .ReverseMap()
+           .ForMember(a => a.Id, opt => opt.MapFrom(s => s.Id != null ? s.Id.ToString() : null))
+           .ForMember(a => a.IconUrl, opt => opt.MapFrom(s => s.Icon != null ? ConfigurationManager.AppSettings["ServiceImages"].ToString() + s.Icon.Name.ToString() : null))
+           .ForMember(a => a.Name, opt => opt.ResolveUsing(new ApplyLanguage(), src => new MappingTranslation(MapperConfig.lang, src.ArabicName, src.EnglishName)))
+           .ForMember(a => a.Description, opt => opt.ResolveUsing(new ApplyLanguage(), src => new MappingTranslation(MapperConfig.lang, src.ArabicDescription, src.EnglishDescription)))
+           .ForMember(a => a.ServiceNote, opt => opt.ResolveUsing(new ApplyLanguage(), src => new MappingTranslation(MapperConfig.lang, src.ServiceNoteAr, src.ServiceNoteEn)))
+          
+             
+
+           ;
+        }
+    }
+}
