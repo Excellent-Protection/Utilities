1090dd15e026cdb310f38f87bf3245fd01ca71b6
EmanMamdouhShaker <eman.mamdoh@excp.sa>
2022-02-28 09:51:03 +0200

    excsetting

---
+++ Controller/ExcSettingController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Utilities.GlobalManagers.CRM;
+using Utilities.Helpers;
+
+namespace Utilities.Controller
+{
+
+    [RoutePrefix("{lang}/api/ExcSetting")]
+    public class ExcSettingController : BaseApiController
+    {
+
+        [HttpGet]
+        [Route("GetSettingsByGroupId")]
+        public HttpResponseMessage GetSettingsByGroupId(string SettingGroupId)
+        {
+            using (ExcSettingManager _mngr = new ExcSettingManager(RequestUtility))
+            {
+                var result = _mngr.GetSettingsByGroupId(SettingGroupId);
+                return Response<Dictionary<string, string>>(result);
+            }
+        }
+    }
+}
---
+++ GlobalRepositories/CRM/ExcSettingRepository.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Xrm.Sdk.Query;
+using Models.CRM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Utilities.DataAccess.CRM;
+using Utilities.Defaults;
+
+namespace Utilities.GlobalRepositories.CRM
+{
+    public class ExcSettingRepository
+    {
+        public List<string> GetSettingIdsByGroupId(string GroupId)
+        {
+            var _service = CRMService.Service;
+
+            var settingQuery = new QueryExpression(CrmEntityNamesMapping.ExcSettings);
+            settingQuery.Criteria.AddCondition("statecode", ConditionOperator.Equal, 0);
+
+            settingQuery.AddLink(CrmEntityNamesMapping.ExcSettingGroups, "new_settinggroup", "new_excsettinggroupid");
+            settingQuery.LinkEntities[0].LinkCriteria.AddCondition("new_excsettinggroupid", ConditionOperator.Equal, GroupId);
+            settingQuery.ColumnSet = new ColumnSet("new_excsettingsid");
+
+            var result = _service.RetrieveMultiple(settingQuery).Entities.Select(a => a.ToEntity<ExcSettings>().Id.ToString()).ToList();
+            return result;
+        }
+    }
+}
