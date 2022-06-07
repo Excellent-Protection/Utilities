9fcdfd298ebbf29350661021a6db679cf8c87551
Samah Hamdy <samah.hamdy@excp.sa>
2022-06-07 16:58:26 +0200

    addLanguageCultureToBaseController
    addLanguageCultureToBaseController

--- Helpers/BaseApiController.cs
+++ Helpers/BaseApiController.cs
@@ -3,12 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.ServiceModel.Channels;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Web.Http;
 using System.Web.Http.Controllers;
+using System.Web.Http.Routing;
+using System.Web.Routing;
+using Utilities.Defaults;
+using Utilities.Enums;
 using Utilities.GlobalManagers.CRM;
 using Utilities.GlobalViewModels;
+using Utilities.Mappers;
 
 namespace Utilities.Helpers
 {
@@ -19,12 +26,54 @@ namespace Utilities.Helpers
         public BaseApiController()
         {
             RequestUtility = new RequestUtility();
-          //  PriceFormate  =int.Parse(new ExcSettingsManager(this.RequestUtility)["PriceFormate"].ToString());
+         
+              //  PriceFormate  =int.Parse(new ExcSettingsManager(this.RequestUtility)["PriceFormate"].ToString());
 
-        }
+            }
 
         protected override void Initialize(HttpControllerContext controllerContext)
         {
+
+            var controller = controllerContext.Controller as BaseApiController; 
+            if (controller != null)
+            {
+                IEnumerable<IHttpRouteData> subroutes = (IEnumerable<IHttpRouteData>)controllerContext.RouteData.Values["MS_SubRoutes"];
+                IHttpRouteData routeData = subroutes.FirstOrDefault();
+                var langRouting = routeData.Values["lang"];
+                switch (langRouting)
+                {
+                    case "en":
+                        {
+                            Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("ar-SA");
+                            Thread.CurrentThread.CurrentUICulture = Thread.CurrentThread.CurrentCulture;
+                           controller.RequestUtility.Language = UserLanguage.English;
+                            break;
+                        }
+                    case "ar":
+                        {
+
+                            Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("ar-SA");
+                            Thread.CurrentThread.CurrentCulture.DateTimeFormat = new System.Globalization.CultureInfo("en-UK").DateTimeFormat;
+                            Thread.CurrentThread.CurrentUICulture = Thread.CurrentThread.CurrentCulture;
+                            controller.RequestUtility.Language = UserLanguage.Arabic;
+                            break;
+                        }
+                    default:
+                        {
+                            Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo(DefaultValues.DefaultLanguageRoute);
+                            Thread.CurrentThread.CurrentCulture.DateTimeFormat = new System.Globalization.CultureInfo("en-UK").DateTimeFormat;
+                            Thread.CurrentThread.CurrentUICulture = Thread.CurrentThread.CurrentCulture;
+                           controller.RequestUtility.Language = DefaultValues.Language;
+                            break;
+                        }
+                }
+
+              // MapperConfig.lang = controller.RequestUtility.RouteLanguage = langRouting ?? (DefaultValues.Language == UserLanguage.Arabic ? DefaultValues.RouteLang_ar : DefaultValues.RouteLang_en);
+
+            }
+
+
+
             base.Initialize(controllerContext);
         }
         protected HttpResponseMessage Response<T>(HttpStatusCodeEnum statusCode, T result)
