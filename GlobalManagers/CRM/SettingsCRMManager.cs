c7f279842c2d867be19fc2f9a128f0b12d8642d1
TasneemEXCP <Tasneem.Salah@excp.sa>
2022-09-14 16:07:33 +0200

    add setting crm

---
+++ GlobalManagers/CRM/SettingsCRMManager.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Xrm.Sdk.Query;
+using Models.CRM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Utilities.DataAccess.CRM;
+using Utilities.Defaults;
+using Utilities.GlobalViewModels.CRM;
+using Utilities.Helpers;
+using Utilities.Mappers;
+
+namespace Utilities.GlobalManagers.CRM
+{
+    public class SettingsCRMManager : BaseManager, IDisposable
+    {
+        internal RequestUtility _requestUtility;
+
+        public SettingsCRMManager(RequestUtility requestUtility) : base(requestUtility)
+        {
+                _requestUtility = RequestUtility;
+        }
+
+        public IEnumerable<SettingCRMVm> this[List<string> keys]
+        {
+            get
+            {
+                var filter = new FilterExpression();
+                filter.FilterOperator = LogicalOperator.Or;
+
+                List<ConditionExpression> Conditions = new List<ConditionExpression>();
+                keys.ForEach(a =>
+                           filter.AddCondition(new ConditionExpression("new_name", ConditionOperator.Equal, a))
+
+               );
+                var _service = CRMService.Service;
+                QueryExpression query = new QueryExpression(CrmEntityNamesMapping.Settings);
+                query.ColumnSet = new ColumnSet("new_value", "new_name");
+
+
+                query.Criteria.AddFilter(filter);
+                var Value = _service.RetrieveMultiple(query).Entities.Select(a => a.ToEntity<SettingCRM>());
+                return Value.ToModelListData<SettingCRMVm>();
+
+            }
+        }
+
+
+
+        public void Dispose()
+        {
+
+        }
+    }
+}
---
+++ Mappers/Profiles/SettingsCRM_SettingCRMVmMappingProfile.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using Models.CRM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Utilities.GlobalViewModels.CRM;
+
+namespace Utilities.Mappers.Profiles
+{
+    class SettingsCRM_SettingCRMVmMappingProfile : Profile
+    {
+        public SettingsCRM_SettingCRMVmMappingProfile()
+        {
+
+            CreateMap<SettingCRMVm, SettingCRM>()
+                 .ForMember(a => a.Key, opt => opt.MapFrom(s => s.Key))
+                .ForMember(a => a.Value, opt => opt.MapFrom(s => s.Value))
+                .ForMember(a => a.Id, opt => opt.Ignore())
+                .ForMember(a => a.SettingId, opt => opt.Ignore())
+                .ForMember(a => a.IsEditable, opt => opt.Ignore())
+                .ForMember(a => a.Description, opt => opt.Ignore())
+                .ForMember(a => a.DataType, opt => opt.Ignore())
+                .ForMember(a => a.LogicalName, opt => opt.Ignore())
+                .ForMember(a => a.Attributes, opt => opt.Ignore())
+                .ForMember(a => a.EntityState, opt => opt.Ignore())
+                .ForMember(a => a.FormattedValues, opt => opt.Ignore())
+                .ForMember(a => a.RelatedEntities, opt => opt.Ignore())
+                .ForMember(a => a.RowVersion, opt => opt.Ignore())
+                .ForMember(a => a.KeyAttributes, opt => opt.Ignore());
+
+        }
+    }
+}
