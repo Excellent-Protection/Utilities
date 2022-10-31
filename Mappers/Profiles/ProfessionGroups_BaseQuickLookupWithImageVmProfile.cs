86b0762479156c925a7ce426cb1c3d0c3df56daa
mahmoudAbdelHamid29 <108276611+mahmoudAbdelHamid29@users.noreply.github.com>
2022-10-31 21:58:59 +0000

    Updating Profession Group
    Reading From new fields in profession group

--- Mappers/Profiles/ProfessionGroups_BaseQuickLookupWithImageVmProfile.cs
+++ Mappers/Profiles/ProfessionGroups_BaseQuickLookupWithImageVmProfile.cs
@@ -20,10 +20,10 @@ namespace Utilities.Mappers.Profiles
       
                .ReverseMap()
                 .ForMember(a => a.Key, o => o.MapFrom(s => s.Id))
-                  .ForMember(a => a.Value, opt => opt.ResolveUsing(new ApplyLanguage(), a => new MappingTranslation(MapperConfig.lang, a.NameAr, a.Attributes.Contains("new_displayenglishname") ? a.Attributes["new_displayenglishname"].ToString() : (a.Name!=null?a.Name:null))))
+                  .ForMember(a => a.Value, opt => opt.ResolveUsing(new ApplyLanguage(), a => new MappingTranslation(MapperConfig.lang, a.PortalNameAr,a.PortalNameEn)))
 
                 .ForMember(a=>a.Image , o=>o.MapFrom(s=>s.ImageUrl!=null ?ConfigurationManager.AppSettings["ProfessionGroupsImagesFolder"].ToString() +s.ImageUrl  :""))
-                .ForMember(a=>a.Description, opt => opt.ResolveUsing(new ApplyLanguage(), a => new MappingTranslation(MapperConfig.lang, a.Description, a.Attributes.Contains("new_descriptionenglish") ? a.Attributes["new_descriptionenglish"].ToString() : null)))
+                .ForMember(a=>a.Description, opt => opt.ResolveUsing(new ApplyLanguage(), a => new MappingTranslation(MapperConfig.lang, a.Description,a.DescriptionEnglish)))
                 ;
         }
     }
